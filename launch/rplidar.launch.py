aa137046f20835a29eff66d8a98b036347d93b07
TesaRamsy <tesaramsy@gmail.com>
2024-06-01 17:07:33 +0900

    Initial commit

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -1,24 +1,44 @@
 #!/usr/bin/env python3
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
+    # LDROBOT LiDAR publisher node
+    ldlidar_node = Node(
+        package='ldlidar_sl_ros2',
+        executable='ldlidar_sl_ros2_node',  # Ensure this is the correct executable name
+        name='ldlidar_publisher_ld14',
+        output='screen',
+        parameters=[
+            {'product_name': 'LDLiDAR_LD14P'},
+            {'laser_scan_topic_name': 'scan'},
+            {'point_cloud_2d_topic_name': 'pointcloud2d'},
+            {'frame_id': 'base_laser'},
+            {'port_name': '/dev/ttyUSB0'},
+            {'serial_baudrate': 230400},
+            {'laser_scan_dir': True},
+            {'enable_angle_crop_func': False},
+            {'angle_crop_min': 135.0},
+            {'angle_crop_max': 225.0}
+        ]
+    )
 
-  #Include LDLidar launch file
-  ldlidar_launch = IncludeLaunchDescription(
-      launch_description_source=PythonLaunchDescriptionSource([
-          get_package_share_directory('ldlidar_sl_ros2'),
-          '/launch/ld14p.launch.py'
-      ])
-  )
+    # Base_link to base_laser transform node
+    base_link_to_laser_tf_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_link_to_base_laser_ld14p',
+        arguments=['0', '0', '0.18', '0', '0', '0', 'base_link', 'base_laser']
+    )
 
-  # Define LaunchDescription variable
-  ld = LaunchDescription()
+    # Define LaunchDescription variable
+    ld = LaunchDescription()
 
-  ld.add_action(ldlidar_launch)
-  
-  return ld
+    # Add nodes to LaunchDescription
+    ld.add_action(ldlidar_node)
+    ld.add_action(base_link_to_laser_tf_node)
+
+    return ld
+
+if __name__ == '__main__':
+    generate_launch_description()
